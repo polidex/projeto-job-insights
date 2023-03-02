0d9154431ab80d565ee6de6c5c8794d2f13f8cd0
polidex <lp.igor@gmail.com>
2023-03-02 15:45:36 -0300

    feat: reqts 3 e 7

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,37 +1,11 @@
 from typing import List, Dict
+from .jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    data = read(path)
+    return list(set(row['industry'] for row in data if row['industry']))
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    raise NotImplementedError
+    return [row for row in jobs if row['industry'] == industry]
