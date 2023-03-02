0a33f4ad4f6dc520ee93216e89e55e3546cf3462
polidex <lp.igor@gmail.com>
2023-03-02 17:08:25 -0300

    feat: reqt 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,11 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = "data/jobs.csv"
+    word = "JS"
+    expected_count = 148
+
+    result = count_ocurrences(path, word)
+
+    assert result == expected_count
